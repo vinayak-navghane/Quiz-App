211cca0a44e227d46a25582c01ee6ee61a355bc2
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-03-11 01:48:28 +0530

    added correct answer display functionality

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,21 @@
 const Result = ({ userAnswers, questions, resetQuiz = () => {} }) => {
-  const correctAnswers = userAnswers.filter((answer) => answer).length;
+  const correctAnswersLen = userAnswers.filter((answer) => answer).length;
+  
 
   return (
     <div className="results">
       <h2>Results</h2>
       <p>
-        You Answered {correctAnswers} out of {questions.length} question
+        You Answered {correctAnswersLen} out of {questions.length} question
         <span onClick={resetQuiz}>Click here to Retry</span>
       </p>
       <ul>
         {questions.map((question, index) => {
+            const correctAnswer = question.answerOptions.find(option => option.isCorrect);
           return (
             <li key={index} data-correct={userAnswers[index]}> 
               Question {index + 1}.{question.question} 
+              <span className="correct-answer"> Correct Answer: {correctAnswer.text}</span>
             </li>
           );
         })}
